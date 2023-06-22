e75bcd6fa7e9175d78c72597f0fc508cb010b838
H3xas <czabajrafal@gmail.com>
2023-06-22 09:18:45 +0200

    feat: add backend pagination

--- server/src/controllers/ledger.controller.ts
+++ server/src/controllers/ledger.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Delete,
   OnModuleInit,
+  Query,
+  BadRequestException,
 } from '@nestjs/common';
 import { CreateLedgerDto } from '../dto/create-ledger.dto';
 import { UpdateLedgerDto } from '../dto/update-ledger.dto';
@@ -20,28 +22,29 @@ import {
   ApiOkResponse,
   ApiOperation,
   ApiParam,
+  ApiQuery,
   ApiTags,
 } from '@nestjs/swagger';
 import {
   InjectInMemoryDBService,
   InMemoryDBService,
 } from '@nestjs-addons/in-memory-db';
-import { initialLedgers, LedgerEntity } from '../db/Ledger';
+import { initialLedgers } from '../db/Ledger';
 import { CategoryEntity } from '../db/Category';
+import { LedgerService } from '../services/ledger.service';
 
 @ApiTags('Ledger')
 @ApiBadRequestResponse()
 @Controller('ledger')
 export class LedgerController implements OnModuleInit {
   constructor(
-    @InjectInMemoryDBService('ledger')
-    private ledgerService: InMemoryDBService<LedgerEntity>,
+    private ledgerService: LedgerService,
     @InjectInMemoryDBService('category')
     private readonly categoryService: InMemoryDBService<CategoryEntity>,
   ) {}
 
   onModuleInit(): any {
-    this.ledgerService.records = initialLedgers;
+    this.ledgerService.initialize(initialLedgers);
   }
 
   @ApiOperation({
@@ -50,13 +53,7 @@ export class LedgerController implements OnModuleInit {
   @ApiCreatedResponse()
   @Post()
   create(@Body() createLedgerDto: CreateLedgerDto) {
-    return this.ledgerService.create({
-      mode: createLedgerDto.mode,
-      title: createLedgerDto.title,
-      amountInCents: createLedgerDto.amountInCents,
-      categoryId: createLedgerDto.categoryId || null,
-      createdAt: Date.now(),
-    });
+    return this.ledgerService.create(createLedgerDto);
   }
 
   @ApiOperation({
@@ -64,11 +61,42 @@ export class LedgerController implements OnModuleInit {
   })
   @ApiNoContentResponse()
   @ApiOkResponse()
+  @ApiQuery({
+    name: 'limit',
+    description: 'The maximum number of records to return',
+    required: false,
+    type: Number,
+  })
+  @ApiQuery({
+    name: 'offset',
+    description: 'The offset number of records to skip',
+    required: false,
+    type: Number,
+  })
   @Get()
-  findAll() {
+  findAll(@Query('limit') limit?: number, @Query('offset') offset?: number) {
+    if (limit !== undefined && offset === undefined) {
+      throw new BadRequestException(
+        'Offset is required when limit is provided',
+      );
+    }
+
+    if (limit === undefined && offset !== undefined) {
+      throw new BadRequestException(
+        'Limit is required when offset is provided',
+      );
+    }
+
+    let records = [];
+
+    if (limit !== undefined && offset !== undefined) {
+      records = this.ledgerService.findAllWithLimitAndOffset(limit, offset);
+    } else {
+      records = this.ledgerService.findAll();
+    }
+
     const categories = this.categoryService.getAll();
-    return this.ledgerService
-      .getAll()
+    return records
       .sort((a, b) => Number(b.createdAt) - Number(a.createdAt))
       .map((record) => {
         return {
@@ -93,7 +121,7 @@ export class LedgerController implements OnModuleInit {
   @ApiOkResponse()
   @Get(':id')
   findOne(@Param('id') id) {
-    return this.ledgerService.get(id);
+    return this.ledgerService.findById(id);
   }
 
   @ApiOperation({
@@ -121,7 +149,7 @@ export class LedgerController implements OnModuleInit {
   @ApiOkResponse()
   @Delete(':id')
   remove(@Param('id') id) {
-    return this.ledgerService.delete(id);
+    return this.ledgerService.deleteById(id);
   }
 
   @ApiOperation({
@@ -131,6 +159,6 @@ export class LedgerController implements OnModuleInit {
   @ApiOkResponse()
   @Delete()
   removeMany(@Body() { ids }: RemoveLedgerDto) {
-    return this.ledgerService.deleteMany(ids);
+    return this.ledgerService.deleteByIds(ids);
   }
 }
